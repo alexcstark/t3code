668304b16c3deac66611ca77d9db853462f77478
Alex Stark <alexcstark@gmail.com>
2026-08-23 11:07:54 -0700

    refactor(web): isolate terminal shell as fork addon

--- apps/web/src/components/ChatView.tsx
+++ apps/web/src/components/ChatView.tsx
@@ -7793,6 +7793,7 @@ export default function ChatView(props: ChatViewProps) {
 
   return (
     <div {...terminalShellRootProps}>
+      {rightPanelOpen && !shouldUseRightPanelSheet ? panelLayoutControls : null}
       <div
         className={cn(
           "flex min-h-0 min-w-0 flex-col overflow-x-hidden",
